d29b6335f0d78ebe3d5115f0c2d95f2d6e2c23c1
Luise Schreiter <s_schreiter1@stud.hwr-berlin.de>
2019-05-07 17:27:32 +0200

    Exercise 3

--- mapper.py
+++ mapper.py
@@ -33,6 +33,9 @@ for line in sys.stdin:
     # strip removes whitespaces and new lines at the beginning and end of the line
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
+    
+    if len(data) < 6:
+        raise Exception('Tuple does not contain 6 elements')
 
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
